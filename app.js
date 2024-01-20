92a63e6bf26a1ef0ed3874f51abf3bb778e6ec06
ialishba4 <alishbahnoman326@gmail.com>
2024-01-20 13:56:09 +0500

    completed

---
+++ app.js
@@ -0,0 +1,156 @@
+// alert("alishba" );
+//  alert("noman" );
+//  alert("dhfuh" );
+//  alert("778990" );
+//  alert("7899" );
+
+//  alert("You are Learning Javascript");
+
+//  alert("HELLOW JAVASCRIPT")
+
+//  var personName = "alishba"
+//  console.log(personName);
+
+//  var percentage = 88
+//  console.log(percentage);
+
+//  var teamName = "traders"
+//  alert(teamName);
+
+//  var bestMan = " charlie"
+//  console.log(bestMan);
+
+//  var caseQty = "144"
+//  var num = " 9"
+//  var sum = (caseQty+num)
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal);
+
+// var productCost = 3.45;
+// var nameOfBrand = gucci;
+
+// var totalMarketing = 1234.9;
+
+// var fullName = alishbah;
+
+// var Alishba67j = illegal;
+// var alishba3 = legal;
+
+
+// var arithmeticOperater = 20 % 6;
+
+// var num = 20 % 6;
+// console.log(num);
+
+// var largeNumber = 200000;
+
+// var popularNumber = 12 - 24;
+// var whatsLeftOver = 9 % 3;
+
+// x = x++;
+
+// var num = 1;
+// var newNum = num++;
+
+// var x = 100;
+// var y = --x;
+// console.log(y);
+
+// var x = 50;
+// var y = x++;
+// console.log(y);
+
+// var y = 50;
+// var z = --y;
+// console.log(z);
+
+
+//  var num = 1;
+//  var newNum = num--;
+
+//  var num = 1;
+//  var newNum = ++num;
+
+//   var num = 78;
+//   var newNum = ++num;
+//    alert(79);
+
+var calculatedNum = 2 + (2 * 6);
+console.log(calculatedNum);
+
+var calculatedNums = (2 + 2) * 6;
+console.log(calculatedNums);
+
+var calculatedNumss = (2 + 2) * (4 + 2);
+console.log(calculatedNumss);
+
+var calculatedNumb = ((2 + 2) * 4) + 2;
+console.log(calculatedNumb);
+
+var calculatedNumbe = (2 + 2) * (4 + 10);
+console.log(calculatedNumbe);
+
+var calculatedNumber = 2 + (2 * 4) + 10;
+console.log(calculatedNumber);
+
+// var num = "2" + "2";
+// var valueNum = "22";
+
+// var message = ("Thanks " + "Dolly" + "");;
+// alert(message);
+
+var message = ("Pakistan " + "Zindabad");;
+alert(message);
+
+var a = 1
+var b = 3
+var c = (a+b)
+console.log(c);
+
+//  var userName = prompt("Enter Your Name");
+//  console.log (userName);
+
+//  var country = prompt("Country?");
+//  console.log (country);
+
+//  var yourName = prompt("Enter Your Name");
+
+var question = "Your species?";
+var defaultAnswer = "human";
+var spec = prompt(question, defaultAnswer);
+
+var timeNow = prompt("what time is it ?")
+console.log(timeNow);
+alert("Thankyou");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
